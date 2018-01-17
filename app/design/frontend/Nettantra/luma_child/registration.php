3dc28a0a789a2781c22ae6600aabb68b454eb690
Aakash <aakash141991@gmail.com>
2018-01-17 18:44:03 +0530

    first commit

---
+++ app/design/frontend/Nettantra/luma_child/registration.php
@@ -0,0 +1,11 @@
+<?php
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+ 
+   \Magento\Framework\Component\ComponentRegistrar::THEME,
+ 
+   'frontend/Nettantra/luma_child',
+ 
+   __DIR__
+ 
+);
